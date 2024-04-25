084c134f4c60512ad295e8ed6ba41e65972d449c
Alex Demetriou <alex.demetriou@comparethemarket.com>
2024-04-25 20:30:29 +0100

    feat: add conform keymap

--- module/lazy/conform.nix
+++ module/lazy/conform.nix
@@ -3,13 +3,33 @@
   lazyPlugin = {
     pkg = pkgs.vimPlugins.conform-nvim;
     opts = {
-      notify_on_error = false;
-      format_after_save.lsp_fallback = true;
+      notify_on_error = true;
+      format_after_save.lsp_fallback = "always";
       formatters_by_ft = {
         lua = [ "stylua" ];
         nix = [ "nixfmt" ];
+        _ = [ "trim_whitespace" ];
+        "*" = [
+          "injected"
+          "codespell"
+        ];
       };
     };
+    keys = {
+      __raw = # lua
+        ''
+          {
+          	"<leader>f",
+          	function()
+          		require("conform").format({
+          			async = true,
+          			lsp_fallback = true,
+          		})
+          	end,
+          }
+        '';
+    };
+    dependencies = [ pkgs.codespell ];
   };
 
   opts.formatexpr = "v:lua.require'conform'.formatexpr()";
