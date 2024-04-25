ab86ce96a5419d13f0400175604ad5b1f46b6b0a
Alex Demetriou <alex.demetriou@comparethemarket.com>
2024-04-25 14:29:54 +0100

    chore(guess-indent): remove some default config lines

--- module/lazy/guess-indent.nix
+++ module/lazy/guess-indent.nix
@@ -2,15 +2,13 @@
 {
   pkg = pkgs.vimPlugins.guess-indent-nvim;
   opts = {
-    auto_cmd = true; # Set to false to disable automatic execution
-    override_editorconfig = false; # Set to true to override settings set by .editorconfig
+    auto_cmd = true;
+    override_editorconfig = false;
     filetype_exclude = [
-      # A list of filetypes for which the auto command gets disabled
       "netrw"
       "tutor"
     ];
     buftype_exclude = [
-      # A list of buffer types for which the auto command gets disabled
       "help"
       "nofile"
       "terminal"
