af0a457febadefcd06af970563d20e56753bd024
Alex Demetriou <alex.demetriou@comparethemarket.com>
2024-04-25 20:30:00 +0100

    chore: sort plugins lists

--- module/lazy/default.nix
+++ module/lazy/default.nix
@@ -1,27 +1,29 @@
 { pkgs, ... }:
 let
-  comment = import ./comment.nix { inherit pkgs; };
-  treesitter = import ./treesitter.nix { inherit pkgs; };
-  conform = import ./conform.nix { inherit pkgs; };
-  gitsigns = import ./gitsigns.nix { inherit pkgs; };
-  lspconfig = import ./lspconfig.nix { inherit pkgs; };
-  telescope = import ./telescope.nix { inherit pkgs; };
   cmp = import ./cmp.nix { inherit pkgs; };
-  guess-indent = import ./guess-indent.nix { inherit pkgs; };
+  comment = import ./comment.nix { inherit pkgs; };
+  conform = import ./conform.nix { inherit pkgs; };
   fzf-lua = import ./fzf-lua.nix { inherit pkgs; };
+  gitsigns = import ./gitsigns.nix { inherit pkgs; };
+  guess-indent = import ./guess-indent.nix { inherit pkgs; };
+  lspconfig = import ./lspconfig.nix { inherit pkgs; };
+  neodev = import ./neodev.nix { inherit pkgs; };
+  telescope = import ./telescope.nix { inherit pkgs; };
+  treesitter = import ./treesitter.nix { inherit pkgs; };
 in
 {
   plugins.lazy = {
     enable = true;
     plugins = [
-      comment
-      treesitter
-      conform.lazyPlugin
-      gitsigns
-      fzf-lua
-      lspconfig
       cmp
+      comment
+      conform.lazyPlugin
+      fzf-lua
+      gitsigns
       guess-indent
+      lspconfig
+      neodev
+      treesitter
     ];
   };
 
