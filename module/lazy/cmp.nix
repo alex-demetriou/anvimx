5240f33e79ba5affbf1dd1d915386f9237562495
Alex Demetriou <alex.demetriou@comparethemarket.com>
2024-05-01 17:04:04 +0100

    feat: add cmp-nvim-lsp-signature-help

--- module/lazy/cmp.nix
+++ module/lazy/cmp.nix
@@ -10,8 +10,8 @@
     pkgs.vimPlugins.cmp-buffer
     pkgs.vimPlugins.cmp-path
     pkgs.vimPlugins.cmp-cmdline
+    pkgs.vimPlugins.cmp-nvim-lsp-signature-help
     pkgs.vimPlugins.cmp-nvim-lua
-    pkgs.vimPlugins.lspkind-nvim
     pkgs.vimPlugins.luasnip
     pkgs.vimPlugins.cmp_luasnip
   ];
@@ -25,10 +25,11 @@
         cmp.setup({
           snippet = {
             expand = function(args)
-              luasnip.lsp_expand(args.body) -- For `luasnip` users.
+              luasnip.lsp_expand(args.body)
             end,
           },
           sources = cmp.config.sources({
+            { name = "nvim_lsp_signature_help" },
             { name = "nvim_lsp" },
             { name = "nvim_lua" },
             { name = "luasnip" },
@@ -37,32 +38,13 @@
             { name = "path" },
           }),
           mapping = cmp.mapping.preset.insert({
-            ['<C-Space>'] = cmp.mapping.complete({}),
+            ['<C-Space>'] = cmp.mapping.complete(),
             ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
             ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
             ['<C-b>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-y>'] = cmp.mapping.confirm({ select = true }),
           }),
-          window = {
-            completion = {
-              winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-              col_offset = -3,
-              side_padding = 0,
-            },
-          },
-          formatting = {
-            fields = { "kind", "abbr", "menu" },
-            format = function(entry, vim_item)
-              local kind = require("lspkind")
-                .cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-              local strings = vim.split(kind.kind, "%s", { trimempty = true })
-              kind.kind = " " .. (strings[1] or "") .. " "
-              kind.menu = "    (" .. (strings[2] or "") .. ")"
-
-              return kind
-            end,
-          },
         })
 
         cmp.setup.cmdline(":", {
